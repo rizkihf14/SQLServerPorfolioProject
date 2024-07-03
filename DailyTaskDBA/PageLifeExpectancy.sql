c40cae40da2361c99dfa69eb0603b7edeb2d811c
Rizki Hari Fitri <142561720+rizkihf14@users.noreply.github.com>
2024-07-03 16:10:37 +0700

    Page Life Expectancy

---
+++ DailyTaskDBA/PageLifeExpectancy.sql
@@ -0,0 +1,9 @@
+SELECT
+    [object_name],
+    counter_name,
+    cntr_value
+FROM
+    sys.dm_os_performance_counters
+WHERE
+    [object_name] LIKE '%Manager%'
+    AND counter_name = 'Page life expectancy'
